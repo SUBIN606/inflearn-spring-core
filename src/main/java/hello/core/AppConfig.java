4e8b1855b24486f396f43a7c180eda75e23eba26
SUBIN LEE <subinlee@SUBINui-MacBookAir.local>
2021-07-02 13:35:28 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,14 +14,25 @@ import hello.core.order.OrderServiceImpl;
 *  애플리케이션 실제 동작에 필요한 구현 객체를 생성해줌
 *  생성자를 통해 의존관계 주입
 * */
+/* 역할과 구현을 메서드 명만 봐도 명확하게 파악할 수 있음 */
 public class AppConfig {
 
+    /* return 값만 변경하면 바로 구현을 변경/확장 할 수 있다! */
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
